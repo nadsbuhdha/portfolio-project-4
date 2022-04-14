8ec17131eadcfd6ef55b5174873d56b72f931307
nadsbuhdha <nadsbuhdha@gmail.com>
2022-04-14 17:22:51 +0000

    album review models added

--- review/models.py
+++ review/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class AlbumReview(models.Model):
+    album_title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
+    album_image = CloudinaryField('image', default='placeholder')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review_auth')
+    album_score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)]) # noqa
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-date_created"]
+
+    def __str__(self):
+         return self.album_title
+        
+    def num_of_likes(self):
+       return self.likes.count()
