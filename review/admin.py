9a83961f1c38b00fcd012b0886426886c9011ab3
nadsbuhdha <nadsbuhdha@gmail.com>
2022-04-15 17:06:20 +0000

    comment admin section added

--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AlbumReview
+from .models import AlbumReview, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -11,6 +11,23 @@ class ReviewAdmin(SummernoteModelAdmin):
     list_filter = ('date_created', 'status')
     list_display = ('album_title', 'slug', 'status', 'date_created')
     search_fields = ['album_title', 'artist']
-    
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    This class defines the appearance and control over the comments in
+    the admin.
+    """
+    list_display = ('name', 'comment_body', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'comment_body']
+    actions = ['approve_comments'] 
+
+    def approve_comments(self, request, queryset):
+        """
+        approval of comments
+        """
+        queryset.update(approved=True)
 
 # admin.site.register(AlbumReview)
--- review/models.py
+++ review/models.py
@@ -34,7 +34,7 @@ class Comment (models.Model):
     album_review = models.ForeignKey(AlbumReview, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
-    body = models.TextField()
+    comment_body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
