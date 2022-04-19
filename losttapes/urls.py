aedc6831b690ad84c137aeaac92cf4d26a003a04
nadsbuhdha <nadsbuhdha@gmail.com>
2022-04-19 08:19:56 +0000

    footer added

--- losttapes/urls.py
+++ losttapes/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('review.urls'), name='review_urls'),
+    path('', include('review.urls'), name='home'),
     path('reviews/', include('review.urls'), name='review_page')
 ]
--- review/urls.py
+++ review/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.IndexPage.as_view(), name="index"),
+    path(" ", views.IndexPage.as_view(), name="home"),
     path("reviews/", views.ReviewPage.as_view(), name="review_page"),
 ]
